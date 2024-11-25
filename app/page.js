4defdfe755c44b153f030788719bb0e81a826854
Shiela04 <shielamarie.daguio@edu.sait.ca>
2024-11-25 11:05:18 -0700

    initial commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import NavigationBar from './components/NavigationBar';
+import BreadCardCarousel from './components/BreadCardCarousel';
+import HeroSection from './components/HeroSection';
+
+export default function Home() {
+  return (
+    <div className="bg-page-bg">
+      <NavigationBar />
+      <HeroSection />
+      <BreadCardCarousel />
+    </div>
+  );
+}
